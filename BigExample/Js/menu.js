37519af496a89294a592d9189275f89dbb25da7d
Khai Nguyen <nguyenkhaik58utc.com>
2020-04-23 01:33:45 +0700

    khai create LayoutTemplate

---
+++ BigExample/Js/menu.js
@@ -0,0 +1,23 @@
+﻿function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("mainlayout").style.marginLeft = "0";
+}
+$(document).ready(function () {
+	$.ajax({
+		url: "menu",
+		type: "Get",
+		contentType: "application/json",
+		success: function (res) {
+			var url = '';
+			for (var i = 0; i < res.length; i++) {
+				url += "<a href='" + res[i].menuUrl + "' >" + res[i].menuName + "</a>";
+
+			}
+			url += "<a href='logout'>Đăng xuất</a>";
+			$("#divMenu").append(url);
+		},
+		error: function () {
+			//alert("error");
+		}
+	});
+});
---
+++ BigExample/Js/sideBar.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+	var heightSize = screen.height - 105;
+	document.getElementById("leftMain").style.height = String(heightSize) + "px";
+	document.getElementById("rightMain").style.height = String(heightSize) + "px";
+	$("#hide-side-bar").on("click", function () {
+		$("#sidebar-container").animate({ opacity: "0", width: "0" });
+	})
+});
+
+function openNav() {
+	$("#sidebar-container").animate({ opacity: "1", width: "14em" });
+}
